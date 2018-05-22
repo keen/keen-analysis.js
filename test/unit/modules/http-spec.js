033e548b541f2714e8b286f270cfe2d6b4f107ef
Adam Kasprowicz <adam.kasprowicz@keen.io>
2018-05-22 11:39:05 +0200

    fix tests http-spec

--- test/unit/modules/http-spec.js
+++ test/unit/modules/http-spec.js
@@ -1,105 +1,109 @@
 import helpers from '../helpers/client-config';
 import KeenClient from '../../../lib/index';
-import parseQueryString from '../helpers/parse-query-string';
-
-// Mock XHR
-const mockXHR = jest.fn();
-window.XMLHttpRequest = () => {};
-window.XMLHttpRequest.prototype.status = 0;
-window.XMLHttpRequest.prototype.headers = {};
-window.XMLHttpRequest.prototype.request = {};
-window.XMLHttpRequest.prototype.open = function(method, url) {
-  this.request = { method, url };
-}
-window.XMLHttpRequest.prototype.setRequestHeader = function(key, value){
-  this.headers[key] = value;
-}
-window.XMLHttpRequest.prototype.send = function(){
-  this.status = 200;
-  this.readyState = 4;
-  this.responseText = helpers.responses.success;
-  this.onreadystatechange();
-  const queryParams = parseQueryString(this.request.url);
-  mockXHR(this.headers, this.request, queryParams);
-}
+import XHRmock from 'xhr-mock';
 
 describe('HTTP methods', () => {
   let client;
 
   beforeEach(() => {
-    mockXHR.mockClear();
+    XHRmock.setup();
     client = new KeenClient(helpers.client);
+  });
+
+  afterEach(() => {
+    XHRmock.teardown();
   });
 
   describe('.get()', () => {
 
-    it('should make a GET request to an API endpoint', () => {
-      client
-        .get(client.url('projectId'))
-        .auth(client.masterKey())
-        .send();
+    it('should make a GET request to an API endpoint', async () => {
 
-      expect(mockXHR)
-        .toBeCalledWith(
-          {
-            Authorization: client.masterKey(),
-            'Content-type': 'application/json'
-          },
-          {
-            method: "GET",
-            url: expect.any(String)
-          },
-          {
-            api_key: expect.any(String),
-            "": "undefined", // todo FIX
-          }
-        );
+      XHRmock.get(`${client.url('projectId')}?api_key=${client.readKey()}`,
+        (req, res) => {
+          expect(req.header('Content-Type')).toEqual('application/json');
+          expect(req.header('Authorization')).toEqual(client.readKey());
+          return res.status(200).body('{}');
+      });
+
+      await client
+              .get(client.url('projectId'))
+              .auth(client.readKey())
+              .send();
     });
 
-    it('should make a GET request with data to an API endpoint', () => {
-      client
-        .get(client.url('queries', 'count'))
-        .auth(client.readKey())
-        .send({
-          event_collection: 'pageview',
-          timeframe: 'this_12_months',
-          timezone: 0
-        });
-      expect(mockXHR).toBeCalledWith(
-        {
-          Authorization: client.readKey(),
-          'Content-type': 'application/json'
-        },
-        {
-          method: "GET",
-          url: expect.any(String)
-        },
-        {}
-      );
-    });
-/*
+    it('should make a GET request with data to an API endpoint', async () => {
 
+      const requestUrl = client.url('queries', 'count');
+      const requestQuery = {
+        event_collection: 'pageview',
+        timeframe: 'this_12_months',
+        timezone: '0'
+      };
+
+      XHRmock.get(new RegExp(requestUrl),
+        (req, res) => {
+          expect(req.header('Content-Type')).toEqual('application/json');
+          expect(req.header('Authorization')).toEqual(client.readKey());
+          requestQuery.api_key = client.readKey();
+          expect(req.url().query).toEqual(requestQuery);
+          return res.status(200).body('{}');
+      });
+
+      await client
+              .get(requestUrl)
+              .auth(client.readKey())
+              .send(requestQuery);
+    });
+
+    it('should make a POST request with data to an API endpoint', async () => {
+
+      const requestUrl = client.url('queries', 'count');
+      const requestQuery = {
+        event_collection: 'pageview',
+        timeframe: 'this_12_months',
+        timezone: '0'
+      };
+
+      XHRmock.post(new RegExp(requestUrl),
+        (req, res) => {
+          expect(req.header('Content-Type')).toEqual('application/json');
+          expect(req.header('Authorization')).toEqual(client.readKey());
+          expect(JSON.parse(req.body())).toEqual(requestQuery);
+          return res.status(200).body('{}');
+      });
+
+      await client
+              .post(requestUrl)
+              .auth(client.readKey())
+              .send(requestQuery);
+    });
+
+    it('should make a PUT request with data to an API endpoint', async () => {
+
+      const requestUrl = client.url('queries', 'count');
+      const requestKey = client.masterKey();
+      const requestQuery = {
+        event_collection: 'pageview',
+        timeframe: 'this_12_months',
+        timezone: '0'
+      };
+
+      XHRmock.put(new RegExp(requestUrl),
+        (req, res) => {
+          expect(req.header('Content-Type')).toEqual('application/json');
+          expect(req.header('Authorization')).toEqual(requestKey);
+          expect(JSON.parse(req.body())).toEqual(requestQuery);
+          return res.status(200).body('{}');
+      });
+
+      await client
+              .put(requestUrl)
+              .auth(requestKey)
+              .send(requestQuery);
+    });
 
   });
 
-  describe('.post()', () => {
 
-    it('should make a POST request with data to an API endpoint', () => {
-      client
-        .post(client.url('queries', 'count'))
-        .auth(client.readKey())
-        .send({
-          event_collection: 'pageview',
-          timeframe: 'this_12_months'
-        })
-        .then(function(res){
-          done();
-        })
-        .catch(function(err){
-          done();
-        });
-    });
-*/
-  });
 
 });
