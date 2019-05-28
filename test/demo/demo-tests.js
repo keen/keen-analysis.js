6f9aa4a459a494ab003803d62795b6afdf306528
Adam Kasprowicz <adam.kasprowicz@keen.io>
2019-05-28 10:53:14 +0200

    security webpack-bundle-analyzer

--- test/demo/demo-tests.js
+++ test/demo/demo-tests.js
@@ -6,6 +6,47 @@ const demoTests = (demoConfig, Keen) => {
   ]
   const client = new Keen(demoConfig);
 
+  client.query('count', {
+    event_collection: 'pageviews',
+    // group_by: 'page.url',
+    interval: 'daily',
+    timeframe: 'this_3_months'
+  }).then(res=>console.log(res))
+
+  return ; 
+  client
+    .get(client.url('queries', 'saved'))
+    .auth(client.masterKey())
+    .send()
+    .then(res => {
+      // Handle response
+      console.log(res);
+    })
+    .catch(err => {
+      // Handle error
+      console.log(err);
+    });
+
+    return;
+
+  client
+        .get(client.url('dashboards'))
+        .send()
+        .then((res) => {
+          AppDispatcher.dispatch({
+            actionType: DashboardConstants.DASHBOARDS_UPDATE,
+            attrs: res
+          })
+          return res
+        })
+        .catch((err) => {
+          console.log('Error fetching dashboards', err)
+          console.log(err.stack)
+        })
+    return;        
+
+
+
 client.query('count', {
   event_collection: 'pageviews',
   group_by: 'page.url',
