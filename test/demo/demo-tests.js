dcec1c0644aef147cda5bd68444b32bc1155af01
Artur Pryka <artur.pryka@keen.io>
2019-07-08 15:05:43 +0200

    update values returned by resultsParser

--- test/demo/demo-tests.js
+++ test/demo/demo-tests.js
@@ -2,15 +2,49 @@ const demoTests = (demoConfig, Keen) => {
   demoConfig.resultParsers= [
     (value) => {
       return Math.round(value);
-    }
+    },
+    value =>  {
+      return Math.round(value) + 10;
+    },
+    value => `last parser: ${value}`,
   ]
   const client = new Keen(demoConfig);
+
+  // client
+  // .query({
+  //   analysisType: 'multi_analysis',
+  //   eventCollection: 'pageviews',
+  //   analyses: {
+  //     'unique users': {
+  //       analysisType: 'count_unique',
+  //       targetProperty: 'keen.id',
+  //     },
+  //     'total visits': {
+  //       analysisType: 'count',
+  //     }
+  //   },
+  //   timeframe: 'this_1117_days',
+  // })
+  // .then(res => {
+  //   // Handle results
+  //   console.log(res);
+  // })
+  // .catch(err => {
+  //   // Handle errors
+  //   console.log(err);
+  // });
+
+  // return;
 
   client.query('count', {
     event_collection: 'pageviews',
     // group_by: 'page.url',
     interval: 'daily',
-    timeframe: 'this_3_months'
+    timeframe: 'this_3_months',
+    includeMetadata: false,
+    metadata: {
+      display_name: 'Purchases (past 2 weeks)',
+    }
   }).then(res=>console.log(res))
 
   return ; 
